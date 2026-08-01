4cef5fd44e846549306dc173a5d3cf4cf0e752b0
sathedhiraj <156566905+sathedhiraj@users.noreply.github.com>
2026-08-01 22:24:38 +0530

    Update page.tsx

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useState, useEffect, use } from "react";
+import { useState, useEffect} from "react";
 import { useRouter, useParams } from "next/navigation";
 import axios from "axios";
 import { AdminLayout } from "../../../components/layout/AdminLayout";
